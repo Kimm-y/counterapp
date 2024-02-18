6108a76da26b736af5a72aa08b22a2c7e785d8c8
Kimm-y <kimmiemumbi789@gmail.com>
2024-02-18 23:35:15 +0300

    portfolio

---
+++ components/Footer/Footer.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+// import InstagramIcon from "";
+
+import "../styles/Footer.css";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <div className="socialMedia">
+        <InstagramIcon /> 
+      </div>
+    
+    </div>
+  );
+}
+
+export default Footer;
